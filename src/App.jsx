ba758dd939b322cf59e64e3b4b2d2780d15a3d24
mehdi rezaei arshad <mehdirezaeiarshad@gmail.com>
2025-03-31 22:26:42 +0330

    getting started

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,35 @@
-import { useEffect, useState } from "react";
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "/src/css.files/App.css";
-import { ScreenSizeProvider, useScreenSize } from "./contexts";
-import {EnglishLanguageContext,EnglishLanguageContextProvider} from "./contexts";
-import { ThemeContext, ThemeContextProvider } from "./contexts";
-import TitlePart from './components/title'  
-import NavBar from "./components/navbar";
-import { motion, useScroll } from "motion/react"
-
-
-function DashBoard() {
-  const { scrollYProgress } = useScroll()
-  const { isMobile } = useScreenSize();
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    if (typeof isMobile !== "undefined") {
-      setIsLoading(false);
-    }
-  }, [isMobile]);
-  return (
-    <>
-    <motion.div
-                id="scroll-indicator"
-                style={{
-                    scaleX: scrollYProgress,
-                    position: "fixed",
-                    bottom: 0,
-                    left: "10%",
-                    right: "10%",
-                    height: 10,
-                    originX: 0,
-                    
-
-                }}
-            />
-      {isLoading ? (
-        <div className="loader"></div>
-      ) : (
-        <>
-          <NavBar />
-          <TitlePart />
-        </>
-      )}
-    </>
-  );
-}
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-    <ThemeContextProvider>
-      <EnglishLanguageContextProvider>
-            <ScreenSizeProvider>
-              <DashBoard />
-            </ScreenSizeProvider>
-      </EnglishLanguageContextProvider>      
-    </ThemeContextProvider>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
